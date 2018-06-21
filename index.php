d87964d5746afdbb7a20d684c03b791248850b71
fanitriastowo <fani.triastowo@gmail.com>
2018-06-21 22:26:08 +0700

    changed to php file

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Recursenews | Programming Blog</title>
+
+	<?php include 'template/css.html'; ?>
+
+	<!-- custom stylesheet here -->
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+</head>
+<body>
+
+<!-- include navbar --> 
+<?php include 'template/navbar.html' ?>;
+
+<!-- Begin page content -->
+<main role="main" class="container">
+  <h1 class="mt-5">Hello World! ¯\_(ツ)_/¯</h1>
+</main>
+
+<!-- include footer -->
+<?php include 'template/footer.html'; ?>
+
+<!-- include js -->
+<?php include 'template/js.html'; ?>
+</body>
+</html>
