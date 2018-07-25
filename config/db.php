18a7c8d393d151d1b859444f3bfc781fab2490cd
fanitriastowo <fani.triastowo@gmail.com>
2018-07-25 22:16:50 +0700

    added htaccess

---
+++ config/db.php
@@ -0,0 +1,11 @@
+<?php
+
+include 'filebase/Document.php';
+include 'filebase/Database.php';
+include 'filebase/Config.php';
+include 'filebase/Format/FormatInterface.php';
+include 'filebase/Format/Json.php';
+include 'filebase/Filesystem.php';
+include 'filebase/Validate.php';
+
+?>
--- test/test_db.php
+++ test/test_db.php
@@ -1,12 +1,6 @@
 <?php 
 
-include '../config/filebase/Document.php';
-include '../config/filebase/Database.php';
-include '../config/filebase/Config.php';
-include '../config/filebase/Format/FormatInterface.php';
-include '../config/filebase/Format/Json.php';
-include '../config/filebase/Filesystem.php';
-include '../config/filebase/Validate.php';
+require '../config/db.php';
 
 use \Filebase\Database;
 use \Filebase\Format\Json;
@@ -21,21 +15,25 @@ $db = new Database([
     'pretty'         => true,
     'safe_filename'  => true,
     'read_only'      => false
-]);
+]); 
 
 echo "starting test" . PHP_EOL;
 echo "<br>";
 echo "<br>";
 echo "<br>";
 
-// $post2 = $db->get('post2');
-// echo "<pre>";
-// var_dump($post2);
-// echo "</pre>";
-
-// echo $post2->title;
-// $post2->tanggal = '25 January 2017';
-// $db->save($post2);
+$post1 = $db->get('post1');
+$post1->tanggal = '25 January 2017';
+$post1->title = 'Lorem Ipsum Dolor';
+$post1->content = "
+	Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+	tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+	quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+	consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+	cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+	proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+";
+//$db->save($post1);
 
 $findAll = $db->findAll();
 echo "<pre>";
