8545b2f47be617aad9061efd22b6fcc1009b4472
fanitriastowo <fani.triastowo@gmail.com>
2018-08-07 21:52:23 +0700

    added bootstrap collapse in archive page

--- archive.php
+++ archive.php
@@ -22,34 +22,92 @@
 
 			<h1>Archive:</h1>
 
-	  	<div id="accordion">
-			  <div class="card">
-			    <div class="card-header" id="headingThree">
-			      <h5 class="mb-0">
-			        <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-			          <h3 class="text-dark">2015</h3>
-			        </button>
-			      </h5>
-			    </div>
-			    <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordion">
-			      <div class="card-body">
-			        <ul>
+			<div id="accordion">
+
+				<div class="card"> <!-- 2018 -->
+					<div class="card-header" id="heading2018">
+						<h5 class="mb-0">
+							<button class="btn btn-link" data-toggle="collapse" data-target="#2018" aria-expanded="true" aria-controls="2018">
+								2018
+							</button>
+						</h5>
+					</div>
+					<div id="2018" class="collapse show" aria-labelledby="heading2018" data-parent="#accordion">
+						<div class="card-body">
+							<ul>
+								<li><a href="#">article 1</a></li>
+								<li><a href="#">article 2</a></li>
+								<li><a href="#">article 3</a></li>
+								<li><a href="#">article 4</a></li>
+							</ul>
+						</div>
+					</div>
+				</div>
+
+				<div class="card">
+					<div class="card-header" id="heading2017">
+						<h5 class="mb-0">
+							<button class="btn btn-link collapsed" data-toggle="collapse" data-target="#2017" aria-expanded="false" aria-controls="2017">
+								2017
+							</button>
+						</h5>
+					</div>
+					<div id="2017" class="collapse" aria-labelledby="heading2017" data-parent="#accordion">
+						<div class="card-body">
+							<ul>
+								<li><a href="#">article 1</a></li>
+								<li><a href="#">article 2</a></li>
+								<li><a href="#">article 3</a></li>
+								<li><a href="#">article 4</a></li>
+							</ul>
+						</div>
+					</div>
+				</div>
+
+				<div class="card">
+					<div class="card-header" id="heading2016">
+						<h5 class="mb-0">
+							<button class="btn btn-link collapsed" data-toggle="collapse" data-target="#2016" aria-expanded="false" aria-controls="2016">
+								2016
+							</button>
+						</h5>
+					</div>
+					<div id="2016" class="collapse" aria-labelledby="heading2016" data-parent="#accordion">
+						<div class="card-body">
+							<ul>
+								<li><a href="#">article 1</a></li>
+								<li><a href="#">article 2</a></li>
+								<li><a href="#">article 3</a></li>
+								<li><a href="#">article 4</a></li>
+							</ul>
+						</div>
+					</div>
+				</div>
+
+				<div class="card">
+					<div class="card-header" id="heading2015">
+						<h5 class="mb-0">
+							<button class="btn btn-link collapsed" data-toggle="collapse" data-target="#2015" aria-expanded="false" aria-controls="2015">
+								2015
+							</button>
+						</h5>
+					</div>
+					<div id="2015" class="collapse" aria-labelledby="heading2015" data-parent="#accordion">
+						<div class="card-body">
+							<ul>
 								<li><a href="#">Janji Programmer</a></li>
 							</ul>
-			      </div>
-			    </div>
-			  </div>
+						</div>
+					</div>
+				</div>
+
 			</div>
+
 		</article>
 
 		<side class="col-3">
-
 			<div class="sidebar">
 				<?php include 'template/sidebar.html'; ?>
-			</div>
-
-			<div class="ads">
-				<?php include 'template/ads.html'; ?>
 			</div>
 		</side>
 
